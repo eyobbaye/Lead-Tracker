d0837b789844bfc5f6e80dadb2390895e1504cb0
eyobbaye <eyob.bay@gmail.com>
2026-09-25 11:16:35 +0300

    fibaseconfig

--- popup.js
+++ popup.js
@@ -4,12 +4,12 @@ import { getDatabase } from "./firebase-database.js";
 
 const firebaseConfig = {
   databaseURL: process.env.DATABASE_URL,
-  apiKey: "AIzaSyD79_z0tdFvAFyDxSooRSF3MQdCEXzARNE",
-  authDomain: "lead-tracker-app-8f73e.firebaseapp.com",
-  projectId: "lead-tracker-app-8f73e",
-  storageBucket: "lead-tracker-app-8f73e.firebasestorage.app",
-  messagingSenderId: "607830310000",
-  appId: "1:607830310000:web:be088e16d2f5db545388b5",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: procss.env.STORAGE_BUCKET,
+  messagingSenderId:MESSAGING_SENDER_ID,
+  appId: APP_ID,
 };
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app)
